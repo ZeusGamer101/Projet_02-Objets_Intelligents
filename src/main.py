37aca2a855c2b448cfb15edf579b7897eac87744
Blondel <106287796+ZeusGamer101@users.noreply.github.com>
2026-04-11 15:55:08 -0400

    [fix] ajout de boucle while dans le main pour garder le programme en continue

--- src/main.py
+++ src/main.py
@@ -3,31 +3,34 @@ from publisher_sensor import client, publish_commande, connecter_mqtt
 from hotword import detecter_hotword
 from stt_module import ecouter_commande
 from TTS import speak
+running = True
 
-if detecter_hotword():
-    speak("je vous écoute")
-    commande = ecouter_commande()
+while running: 
+    if detecter_hotword():
+        speak("je vous écoute")
+        commande = ecouter_commande()
 
-    intent = detecter_intention(commande)
+        intent = detecter_intention(commande)
 
-    print("Intention :", intent)
+        print("Intention :", intent)
 
-    if intent != "inconnue":
-        publish_commande(intent)
-        if intent == "on":
-            speak("j'allume la lampe")
+        if intent != "inconnue":
+            publish_commande(intent)
+            if intent == "on":
+                speak("j'allume la lampe")
 
-        elif intent == "off":
-            speak("j'éteins la lampe")
+            elif intent == "off":
+                speak("j'éteins la lampe")
 
-        elif intent == "clignoter":
-            speak("j'active le clignotement")
+            elif intent == "clignoter":
+                speak("j'active le clignotement")
 
-        elif intent == "mode nuit":
-            speak("mode nuit activé")
+            elif intent == "mode nuit":
+                speak("mode nuit activé")
 
-        elif intent == "etat":
-            #speak("je vérifie l'état de la lampe")
-            pass
-    else:
-        speak("commande non reconnue")
+            elif intent == "etat":
+                #speak("je vérifie l'état de la lampe")
+                pass
+        else:
+            speak("commande non reconnue")
+    
